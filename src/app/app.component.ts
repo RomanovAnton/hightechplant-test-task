156d2f128604e6c9e2101d502da6e23c81849e55
Anton Romanov <rad250495@yandex.ru>
2023-08-13 20:39:02 +0700

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hightechplant-test-task';
+
+  list = [
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+    { name: 'asd', age: 15 },
+  ];
+}
